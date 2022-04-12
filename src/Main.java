718adf4aae24bfdb77ba3712d22735d560cbad64
n11024437 <n11024437@qut.edu.au>
2022-04-13 08:29:11 +1000

    Added work to get GUI visible. GUI not yet Visible.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+public class Main {
+
+
+    public static void main(String args[]){
+        TitleScreen gui = new TitleScreen("Main Screen");
+    }
+}
--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -5,8 +5,28 @@ import java.awt.event.ActionListener;
 
 public class TitleScreen extends JFrame implements  ActionListener, Runnable {
 
-    public static final int WIDTH = 300;
-    public static final int HEIGHT = 200;
+    public static final int WIDTH = 3000;
+    public static final int HEIGHT = 2000; // set these to have a consistent size between screens
+
+    private JPanel pnlOne;
+
+    private JPanel createPanel (Color c) {
+        var panel = new JPanel();
+        panel.setBackground(c);
+        return panel;
+    }
+
+    private void CreateGUI(){
+        setSize(WIDTH,HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        pnlOne  = createPanel(Color.white);
+
+        //panel related code to go here
+        pack();
+        setVisible(true);
+    }
+
 
     public TitleScreen(String title) throws HeadlessException {
         super(title);
@@ -21,4 +41,7 @@ public class TitleScreen extends JFrame implements  ActionListener, Runnable {
     public void run() {
 
     }
+
+
+
 }
