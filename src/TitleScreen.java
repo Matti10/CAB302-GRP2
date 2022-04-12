f211374380212a60b5411b5788306990e6b5e015
n11024437 <n11024437@qut.edu.au>
2022-04-12 12:42:29 +1000

    Install Title screen commit

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -1,2 +1,24 @@
-public class TitleScreen {
+import  java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TitleScreen extends JFrame implements  ActionListener, Runnable {
+
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 200;
+
+    public TitleScreen(String title) throws HeadlessException {
+        super(title);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
