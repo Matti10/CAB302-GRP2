87f0b51e931e88fffca7f3d14c074316d3a1615e
n11024437 <n11024437@qut.edu.au>
2022-04-25 09:13:40 +1000

    Set gui Standard size which the user cannot resize. Added three buttons with no functionality. Added white panel to be moved and resized to display previous mazes

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -5,11 +5,12 @@ import java.awt.event.ActionListener;
 
 public class TitleScreen extends JFrame implements  ActionListener, Runnable {
 
-    public static final int WIDTH = 1000;
-    public static final int HEIGHT = 1000; // set these to have a consistent size between screens
+    public static final int WIDTH = 500;
+    public static final int HEIGHT = 500; // set these to have a consistent size between screens
 
-    private JPanel pnlOne;
+    private JPanel pnlOne; //Background panel
     private JPanel MazeDisplayPanel;
+    private JPanel ButtonShelf; // panel to store buttons
 
     private JPanel createPanel (Color c) {
         var panel = new JPanel();
@@ -19,22 +20,44 @@ public class TitleScreen extends JFrame implements  ActionListener, Runnable {
     }
 
     private void CreateGUI(){
+
         JButton NewMazeButton = new JButton("New Maze");
+
         JButton RandomMazeButton = new JButton("Random Maze");
+
         JButton LoadButton = new JButton("Load Maze");
 
+        GridLayout buttonLayout = new GridLayout(3,1); // layout for ButtonShelf
+
+        GridLayout pageLayout = new GridLayout(3, 2); // layout for page
+
+        buttonLayout.setVgap(40);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-        pnlOne  = createPanel(Color.darkGray); //background
+
+        pnlOne  = createPanel(Color.darkGray); //background Panel
+
+        ButtonShelf = createPanel(Color.GRAY);
+
         MazeDisplayPanel = createPanel(Color.white);
+
         pnlOne.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        MazeDisplayPanel.setBorder(BorderFactory.createEmptyBorder(300, 30, 10, 30));
+
+        MazeDisplayPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+
+        ButtonShelf.setLayout(buttonLayout);
+
+        pnlOne.add(MazeDisplayPanel); // added panel to display list of recent mazes
+
+        pnlOne.add(ButtonShelf);
 
 
-        pnlOne.add(MazeDisplayPanel);
-        pnlOne.add(RandomMazeButton);
-        pnlOne.add(NewMazeButton);
-        pnlOne.add(LoadButton);
+
+        ButtonShelf.add(RandomMazeButton);
+
+        ButtonShelf.add(NewMazeButton);
+
+        ButtonShelf.add(LoadButton);
 
         //panel related code to go here
         getContentPane().add(pnlOne,BorderLayout.CENTER);
