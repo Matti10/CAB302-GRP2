7035a27ecca74026db333107842624e6e1ad351a
n11024437 <n11024437@qut.edu.au>
2022-05-28 09:31:08 +1000

    Added function to create maze cell

--- src/Gui/GUIutilities.java
+++ src/Gui/GUIutilities.java
@@ -11,4 +11,38 @@ public class GUIutilities {
 
         return panel;
     }
+
+    public void CreateCell(){
+       JPanel background = createPanel(Color.white); // background of maze cell
+        background.setLayout(new GridLayout(3,3));
+
+        JPanel cellCorner1 = createPanel(Color.black);
+        JPanel cellNorth = createPanel(Color.white);
+        JPanel cellCorner2 = createPanel(Color.black);
+        JPanel cellWest = createPanel(Color.white);
+        JPanel cellCorner3 = createPanel(Color.black);
+        JPanel cellEast = createPanel(Color.white);
+        JPanel cellCorner4= createPanel(Color.black);
+        JPanel cellSouth = createPanel(Color.white);
+        JPanel cellCenter = createPanel(Color.white);
+
+        // each of the cells will occupy a 3x3 grid
+
+        background.add(cellCorner1);
+        background.add(cellNorth);
+        background.add(cellCorner2);
+        background.add(cellWest);
+        background.add(cellCorner3);
+        background.add(cellEast);
+        background.add(cellCorner4);
+        background.add(cellSouth);
+        background.add(cellCenter);
+
+    }
+
+
+
+
 }
+
+
