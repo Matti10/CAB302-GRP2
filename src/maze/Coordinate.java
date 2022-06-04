b211c0a3b7e62d947c9f81718e6fcd91e14a7ae0
Matti10 <mattias.winsen@outlook.com.au>
2022-06-04 12:32:48 +1000

    quick coord tidy up

--- src/maze/Coordinate.java
+++ src/maze/Coordinate.java
@@ -1,15 +1,12 @@
 package maze;
 
 public class Coordinate {
-    public int y;
     public int x;
+    public int y;
 
 
     public Coordinate(int x, int y)
     {
-//        super(3,3,false/*, new Coordinate(0,0),new Coordinate(0,0)*/); //not sure about this...
-
-
         this.x = x;
         this.y = y;
     }
