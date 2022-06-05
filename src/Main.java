acedc775791d951c0ff0a9a6b23a136ed727cc46
Matti10 <mattias.winsen@outlook.com.au>
2022-06-05 14:23:25 +1000

    randomMazev2 progress, random solution working.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import GUI.Chunk;
 import GUI.TitleScreen;
 import db.MazeListData;
 import maze.*;
+import java.util.*;
+
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,12 +13,20 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         MazeListData mazeData = new MazeListData();
 
-        Maze testMaze = Maze.initMaze(11, 11, true, 0, 0, 9, 9, "someName", mazeData);
-        testMaze.randomMaze();
+        int len = 10;
 
+        Maze testMaze = Maze.initMaze(len, len, false, 1, 0, 9, 3, "someName", mazeData);
+
+        List<Coordinate> noWalls = testMaze.setRandomSolution(0.8);
+
+        System.out.print(noWalls.toString());
+        for (Coordinate move : testMaze.getFirstSolution()) {
+           System.out.print(move.toString());
+        }
 
         TitleScreen titleScreen = new TitleScreen();
         titleScreen.CreateGUI();
         titleScreen.AddMaze(testMaze);
+
     }
 }
